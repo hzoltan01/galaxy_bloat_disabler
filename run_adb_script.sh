8535817296990329b2dc9e9fb2f1a0799b636099
Zoltán Hellmann <hellmann.zoltan01@gmail.com>
2025-10-12 21:11:24 +0200

    Handling missing device error

--- run_adb_script.sh
+++ run_adb_script.sh
@@ -1,5 +1,11 @@
 #/!usr/bin/env bash
 
+# Looks for a line ending with device
+if ! adb devices | grep -q "device\$"; then
+    print 'No ADB device found'
+    exit 1
+fi
+
 adb push disable_bloatware.sh /data/local/tmp
 adb shell chmod +x /data/local/tmp/disable_bloatware.sh
 adb shell sh /data/local/tmp/disable_bloatware.sh
