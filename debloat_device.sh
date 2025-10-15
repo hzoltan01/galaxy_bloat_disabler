7b5844eea68f44f26f1071b2e24281d0c82c85c3
Zoltán Hellmann <hellmann.zoltan01@gmail.com>
2025-10-15 22:25:20 +0200

    selectable package categories

--- debloat_device.sh
+++ debloat_device.sh
@@ -1,11 +1,51 @@
-#/!usr/bin/env bash
+#!/usr/bin/env bash
+
+adb_error() {
+    echo "$1"
+    exit 1
+}
+
+unnecessary='
+com.facebook.services
+com.facebook.system
+com.facebook.appmanager
+com.sem.factoryapp
+de.telekom.tsc
+com.aura.oobe.samsung
+com.samsung.android.ipsgeofence
+com.samsung.android.mapsagent
+com.samsung.android.app.omcagent
+com.samsung.android.sm.devicesecurity
+com.samsung.android.rubin.app
+com.android.providers.partnerbookmarks
+com.sec.android.diagmonagent
+'
+
+spyware='
+com.hiya.star
+com.samsung.android.fast
+com.android.chrome
+com.sec.android.app.chromecustomizations
+'
+
+bloatware='
+com.microsoft.skydrive
+com.microsoft.appmanager
+com.samsung.android.mdx
+com.google.android.youtube
+com.google.android.apps.tachyon
+de.axelspringer.yana.zeropage
+'
+
+echo "$unnecessary" > package_list.txt
+[[ "$@" =~ "spyware" ]] && echo "$spyware" >> package_list.txt
+[[ "$@" =~ "bloatware" ]] && echo "$bloatware" >> package_list.txt
+
 
 # Looks for a line ending with device
-if ! adb devices | grep -q "device\$"; then
-    print 'No ADB device found'
-    exit 1
-fi
+adb devices | grep -q "device\$" || adb_error 'No ADB device found'
 
-adb push disable_bloatware.sh /data/local/tmp
-adb shell chmod +x /data/local/tmp/disable_bloatware.sh
-adb shell sh /data/local/tmp/disable_bloatware.sh
+adb push disable_bloatware.sh /data/local/tmp || adb_error "Can't push disable_bloatware.sh to /data/local/tmp"
+adb push package_list.txt /data/local/tmp || adb_error "Can't push package_list.txt to /data/local/tmp"
+adb shell chmod +x /data/local/tmp/disable_bloatware.sh || adb_error "An error occurred while trying to execute adb shell"
+adb shell sh /data/local/tmp/disable_bloatware.sh || adb_error "An error occurred while trying to execute adb shell"
--- disable_bloatware.sh
+++ disable_bloatware.sh
@@ -1,60 +1,33 @@
 #!/system/bin/sh
 
-# Put this script to /data/local/tmp to be able to make it executable and run it
-
-packages='
-com.hiya.star
-com.facebook.services
-com.facebook.system
-com.facebook.appmanager
-com.microsoft.skydrive
-com.microsoft.appmanager
-com.sem.factoryapp
-de.axelspringer.yana.zeropage
-de.telekom.tsc
-com.google.android.youtube
-com.google.android.apps.tachyon
-com.google.android.safetycore
-com.aura.oobe.samsung
-com.samsung.android.fast
-com.samsung.android.ipsgeofence
-com.samsung.android.mapsagent
-com.samsung.android.app.omcagent
-com.samsung.android.sm.devicesecurity
-com.samsung.android.mdx
-com.samsung.android.rubin.app
-com.android.providers.partnerbookmarks
-com.android.chrome
-com.sec.android.app.chromecustomizations
-com.sec.android.diagmonagent
-'
-
 # Disabling every package if installed
-for package in $packages; do
+while read package; do
     if pm path $package > /dev/null 2>&1; then
-        pm disable-user --user 0 $package || echo "Can't disable $package"
+        pm disable-user --user 0 $package || pm disable --user 0 $package || echo "Can't disable $package"
     fi
-done
+done < /data/local/tmp/package_list.txt
 
 # Removing unnecessary settings
 
 # User manual settings page
 settings put global online_manual_url 0
+echo 'User manual from system settings is removed'
 # Remote control settings page
 settings put system remote_control 0
+echo 'Remote control from system settings is removed'
 
 
 # Disable telemetry
 
 # Play protect - Disable
-settings put global package_verifier_user_consent -1
+settings put global package_verifier_user_consent -1 && echo 'Play Protect disabled'
 # Personal data intelligence - Disable
-settings put global sss_enabled 0
+settings put global sss_enabled 0 && echo 'Personal data and intelligence disabled'
 # Send diagnostic data - Disable
-settings put system samsung_errorlog_agree 0
+settings put system samsung_errorlog_agree 0 && echo 'Send diagnostic data disabled'
 # Android personalization service - Disable
-settings put secure content_capture_enabled 0
+settings put secure content_capture_enabled 0 && echo 'Android personalization service disabled'
 # Usage and diagnostics - Disable
-settings put global multi_cb 0
-# Galaxy AI local only - Enable
-settings put system prevent_online_processing 1
+settings put global multi_cb 0 && echo 'Usage and diagnostics disabled'
+# Process data only on device (Galaxy AI) - Enable
+settings put system prevent_online_processing 1 && echo 'Process data only on device enabled (Galaxy AI)'
